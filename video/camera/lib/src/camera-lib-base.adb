7846808d0244103a371690f99f0f7c72faef3e68
wbullaughey <wlb122@verizon.net>
2024-07-27 16:55:09 -0400

    first checkin move base to lib

--- video/camera/lib/src/camera-lib-base.adb
+++ video/camera/lib/src/camera-lib-base.adb
@@ -9,6 +9,7 @@ package body Camera.Lib.Base is
 
    use type Ada_Lib.Time.Time_Type;
    use type Data_Type;
+   use type Gnoga.Gui.Plugin.jQueryUI.Widget.Dialog_Access;
    use type Index_Type;
    use type Value_Type;
 
@@ -100,6 +101,93 @@ package body Camera.Lib.Base is
    end Close;
 
    ---------------------------------------------------------------
+   procedure Close_Message_Box (
+      Connection_Data            : in out Connection_Data_Type) is
+   ---------------------------------------------------------------
+
+      Message                    : constant String :=
+                                    "message box dooes not exist";
+
+   begin
+      Log_In (Debug);
+      if Connection_Data.Message_Box_Dialog = Null then
+         Log_Here (Debug, Message);
+         Log_Exception (Debug, Message);
+         raise Failed with Message;
+      else
+         Connection_Data.Message_Box_Dialog.Fire_On_Close;
+      end if;
+      Log_Out (Debug);
+   exception
+
+      when Fault: others =>
+         Trace_Exception (Debug, Fault);
+         Log_Exception (Debug, Fault);
+         raise;
+
+   end Close_Message_Box;
+
+   ----------------------------------------------------------------
+   function Get_Adjust_Card (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Widgets.Adjust.Adjust_Card_Access is
+   ----------------------------------------------------------------
+
+   begin
+      return Connection_Data.Main_Data.Get_Adjust_Card;
+   end Get_Adjust_Card;
+
+   ----------------------------------------------------------------
+   function Get_Cards (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Main.Cards_Access_Type is
+   ----------------------------------------------------------------
+
+   begin
+      return Connection_Data.Main_Data.Get_Cards;
+   end Get_Cards;
+
+   ----------------------------------------------------------------
+   function Get_Configured_Card (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Widgets.Configured.Configured_Card_Access is
+   ----------------------------------------------------------------
+
+   begin
+      return Connection_Data.Main_Data.Get_Configured_Card;
+   end Get_Configured_Card;
+
+   ----------------------------------------------------------------
+   function Get_Control_Card (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Widgets.Control.Control_Card_Access is
+   ----------------------------------------------------------------
+
+   begin
+      return Connection_Data.Main_Data.Get_Control_Card;
+   end Get_Control_Card;
+
+   ----------------------------------------------------------------
+   function Get_Exit_Button (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Gnoga.Gui.Element.Common.Pointer_To_Button_Class is
+   ----------------------------------------------------------------
+
+   begin
+      return Connection_Data.Main_Data.Get_Exit_Button;
+   end Get_Exit_Button;
+
+   ----------------------------------------------------------------
+   function Get_Main_Window (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Gnoga.Gui.Window.Pointer_To_Window_Class is
+   ----------------------------------------------------------------
+
+   begin
+      return Connection_Data.Main_Data.Get_Main_Window;
+   end Get_Main_Window;
+
+   ---------------------------------------------------------------
    procedure Get_Response (
       Camera                     : in out Base_Camera_Type;
       Expect_Ack                 : in     Boolean;
@@ -182,6 +270,24 @@ package body Camera.Lib.Base is
       Log_Out (Debug);
    end Get_Response;
 
+   ----------------------------------------------------------------
+   function Get_Tabs (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Gnoga.Gui.View.Card.Pointer_To_Tab_Class is
+   ----------------------------------------------------------------
+
+   begin
+      return Connection_Data.Main_Data.Get_Tabs;
+   end Get_Tabs;
+
+   ----------------------------------------------------------------
+   procedure Halt is
+   ----------------------------------------------------------------
+
+   begin
+      Log_Here (Debug);
+   end Halt;
+
    ---------------------------------------------------------------
    overriding
    procedure Host_Open (
@@ -201,6 +307,16 @@ package body Camera.Lib.Base is
          Camera_No_Found (Host_Address, Port, Fault);
 
    end Host_Open;
+
+   ----------------------------------------------------------------
+   procedure Initialize (
+      Connection_Data            : in out Connection_Data_Type) is
+   ----------------------------------------------------------------
+
+   begin
+      Log_Here (Debug);
+      Connection_Data.Main_Data := new Main.Main_Data_Type;
+   end Initialize;
 
    ---------------------------------------------------------------
    overriding
@@ -226,6 +342,29 @@ package body Camera.Lib.Base is
          Camera_No_Found (Address, Port, Fault);
 
    end IP_Open;
+
+   ----------------------------------------------------------------
+   procedure Message_Box (
+      Connection_Data            : in out Connection_Data_Type;
+      Title, Text                : in     String;
+      Style                      : in     Gnoga.Gui.Plugin.Message_Boxes.
+                                             Message_Box_Type :=
+                                             Gnoga.Gui.Plugin.Message_Boxes.OK_Box) is
+   ----------------------------------------------------------------
+
+      Message_Box_Dialog         : aliased Gnoga.Gui.Plugin.jQueryUI.
+                                    Widget.Dialog_Type;
+
+   begin
+      Log_In (Debug, Quote ("text", Text));
+      Connection_Data.Message_Box_Dialog := Message_Box_Dialog'unchecked_access;
+      Connection_Data.Message_Box_Result :=
+         Gnoga.Gui.Plugin.Message_Boxes.Message_Box (
+            Message_Box_Dialog, Connection_Data.Main_Window.all,
+            Title, Text, Style);
+      Log_Out (Debug, "result " & Connection_Data.Message_Box_Result'img);
+      Connection_Data.Message_Box_Dialog := Null;
+   end Message_Box;
 
    ---------------------------------------------------------------
    overriding
@@ -303,6 +442,25 @@ package body Camera.Lib.Base is
 
    end Process_Command;
 
+   ----------------------------------------------------------------
+   procedure Reset_Update_Event (
+      Connection_Data            : in out Connection_Data_Type) is
+   ----------------------------------------------------------------
+
+   begin
+      Log_Here (Debug);
+      Connection_Data.Update_Event.Reset_Event;
+   end Reset_Update_Event;
+
+   ---------------------------------------------------------------
+   procedure Trigger_Update_Event (
+      Connection_Data            : in out Connection_Data_Type) is
+   ---------------------------------------------------------------
+   begin
+      Log_Here (Debug);
+      Connection_Data.Update_Event.Set_Event;
+   end Trigger_Update_Event;
+
    ---------------------------------------------------------------
    overriding
    procedure URL_Open (
@@ -330,6 +488,16 @@ package body Camera.Lib.Base is
          end;
 
    end URL_Open;
+
+   ---------------------------------------------------------------
+   procedure Wait_For_Update_Event (
+      Connection_Data            : in out Connection_Data_Type) is
+   ---------------------------------------------------------------
+
+   begin
+      Connection_Data.Update_Event.Wait_For_Event;
+      Log_Here (Debug);
+   end Wait_For_Update_Event;
 
    ---------------------------------------------------------------
    procedure Write (
@@ -362,6 +530,7 @@ package body Camera.Lib.Base is
    end Write;
 
 begin
+   Include_Task := True;
 --Debug := True;
-   Log_Here (Elaborate or Trace_Options);
+   Log_Here (Debug or Elaborate or Trace_Options);
 end Camera.Lib.Base;
--- video/camera/lib/src/camera-lib-base.ads
+++ video/camera/lib/src/camera-lib-base.ads
@@ -1,6 +1,18 @@
+with Ada_Lib.Event;
+with Ada_Lib.GNOGA;
 with Ada_Lib.Socket_IO.Client;
-with Configuration.Camera;
+with Camera.Commands;
+with Configuration.Camera.State;
 with GNAT.Sockets;
+with Gnoga.Gui.Element.Common;
+with Gnoga.Gui.Plugin.jQueryUI.Widget;
+with Gnoga.Gui.Plugin.Message_Boxes;
+with Gnoga.Gui.View.Card;
+with Gnoga.Gui.Window;
+with Main;
+with Widgets.Adjust;
+with Widgets.Configured;
+with Widgets.Control;
 
 package Camera.Lib.Base is
 
@@ -8,6 +20,86 @@ package Camera.Lib.Base is
    Timed_Out                     : exception;
 
    use type GNAT.Sockets.Port_Type;
+
+   type Mouse_Click_Action_Type  is (Any_Scroll, Horizontal_Scroll,
+                                       No_Action, No_Change, Vertical_Scroll);
+
+   type Connection_Data_Type     is new Ada_Lib.GNOGA.Connection_Data_Type with
+                                    record
+      Camera                     : Standard.Camera.Commands.Camera_Class_Access :=
+                                    Null;
+      Camera_Pan                 : Standard.Camera.Commands.Absolute_Type;
+      Camera_Pan_Speed           : Standard.Camera.Commands.Property_Type;
+      Camera_Tilt                : Standard.Camera.Commands.Absolute_Type;
+      Camera_Tilt_Speed          : Standard.Camera.Commands.Property_Type;
+      Camera_Zoom                : Standard.Camera.Commands.Property_Type;
+      Main_Data                  : access Main.Main_Data_Type;
+      Message_Box_Dialog         : Gnoga.Gui.Plugin.jQueryUI.Widget.
+                                    Dialog_Access := Null;
+      Message_Box_Result         : Gnoga.Gui.Plugin.Message_Boxes.
+                                    Message_Box_Result;
+      Mouse_Action               : Mouse_Click_Action_Type := No_Action;
+                                    -- application responing to
+      State                      : Configuration.Camera.State.State_Type;
+      Update_Event               : Ada_Lib.Event.Event_Type (
+                                    new String'("update event"));
+   end record;
+
+   type Connection_Data_Access   is access all Connection_Data_Type;
+   type Connection_Data_Class_Access
+                                 is access all Connection_Data_Type'class;
+
+   procedure Close_Message_Box (
+      Connection_Data            : in out Connection_Data_Type);
+
+   function Get_Adjust_Card (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Widgets.Adjust.Adjust_Card_Access;
+
+   function Get_Cards (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Main.Cards_Access_Type;
+
+   function Get_Configured_Card (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Widgets.Configured.Configured_Card_Access;
+
+   function Get_Control_Card (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Widgets.Control.Control_Card_Access;
+
+   function Get_Exit_Button (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Gnoga.Gui.Element.Common.Pointer_To_Button_Class;
+
+   function Get_Main_Window (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Gnoga.Gui.Window.Pointer_To_Window_Class;
+
+   function Get_Tabs (
+      Connection_Data            : in out Connection_Data_Type
+   ) return Gnoga.Gui.View.Card.Pointer_To_Tab_Class;
+
+   procedure Halt;
+
+   procedure Initialize (
+      Connection_Data            : in out Connection_Data_Type);
+
+   procedure Message_Box (
+      Connection_Data            : in out Connection_Data_Type;
+      Title, Text                : in     String;
+      Style                      : in     Gnoga.Gui.Plugin.Message_Boxes.
+                                             Message_Box_Type :=
+                                             Gnoga.Gui.Plugin.Message_Boxes.OK_Box);
+
+   procedure Reset_Update_Event (
+      Connection_Data            : in out Connection_Data_Type);
+
+   procedure Trigger_Update_Event (
+      Connection_Data            : in out Connection_Data_Type);
+
+   procedure Wait_For_Update_Event (
+      Connection_Data            : in out Connection_Data_Type);
 
    type Commands_Type is (
       Auto_Focus,
