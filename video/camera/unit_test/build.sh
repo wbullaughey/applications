6a05fa6776fd34a859f08c129838294290f6fabf
wbullaughey <wlb122@verizon.net>
2025-01-17 13:35:27 -0500

    merge to desktop 1/17/25

--- video/camera/unit_test/build.sh
+++ video/camera/unit_test/build.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 source ~/.zshrc
 echo building on $OS_VERSION
+export LOCATION=$1
 export RELATIVE_PATH="Project/git/alr/applications/video/camera/unit_test"
 export BUILD_PATH="/Volumes/wayne/$RELATIVE_PATH"
 echo BUILD_PATH $BUILD_PATH
@@ -19,7 +20,23 @@ case  ${OS_VERSION%%.*} in
    *)
       export OUTPUT=build.txt
       echo laptop 2>&1 | tee $OUTPUT
-      cd /Users/wayne/Project/git/alr/applications/video/camera/unit_test
+      case $LOCATION in
+
+         "local")
+            echo local build
+            ;;
+
+         "remote")
+            echo remote build
+            cd /Users/wayne/Project/git/alr/applications/video/camera/unit_test
+            ;;
+
+         *)
+            echo "invalid build location $LOCATION"
+            exit
+            ;;
+
+      esac
       pwd 2>&1 | tee -a $OUTPUT
       echo "path=$PATH"
       which alr
